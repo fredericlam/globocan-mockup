5649253d86c282cf067c7c8e034fa17284b52fb3
LAM Frédéric <flaaam@gmail.com>
2015-06-12 09:27:39 +0200

    Mockup 1 - new desing globocan 2012

---
+++ mockup-2/js/main.js
@@ -0,0 +1,19 @@
+	
+	$(document).ready( function(){
+
+		console.log(" Page loaded ") ;
+
+	});
+
+	$( window ).scroll(function() {
+
+  		if( $(this).scrollTop() >= 50 )
+  		{
+   			$('nav.navbar').addClass('navbar-fixed-top').css('margin-top',0) ; 
+  		}
+  		else
+  		{
+  			$('nav.navbar').removeClass('navbar-fixed-top').css('margin-top','10px') ; 
+  		}
+	});
+
